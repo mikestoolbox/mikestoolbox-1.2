1a495eeb5158a240212398382da3f9ffb05f4f52
mikestoolbox <mikestoolbox@pobox.com>
2024-06-20 11:42:21 -0700

    Remove unused CreateMask function

--- src/Misc.cpp
+++ src/Misc.cpp
@@ -42,19 +42,6 @@
 
 namespace mikestoolbox {
 
-static inline uintsys CreateMask (uintsys u)
-{
-    u -= 1;;
-    u |= (u >>  1);
-    u |= (u >>  2);
-    u |= (u >>  4);
-    u |= (u >>  8);
-    u |= (u >> 16);
-    u |= (u >> 32);
-
-    return u;
-}
-
 String Latin1ToUTF8 (const String& str)
 {
     String str_Result;
